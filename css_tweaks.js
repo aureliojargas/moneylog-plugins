47642fae13f64afa680d0bf319293f475609b5bf
Leandro Voltolino <xupisco@gmail.com>
2012-02-06 14:55:37 -0200

    dark editor

--- css_tweaks.js
+++ css_tweaks.js
@@ -8,9 +8,29 @@ var css_app_flavor = {
     'margin-top': '-6px'
 }
 
+var css_editor_data = {
+    'background-color': '#222',
+    'color': '#EEE',
+    'line-height': '16px',
+    'font-size': '12px',
+    'padding': '15px',
+}
+
+var css_editor_data_wrapper = {
+    'border': '0',
+    'top': '5px',
+    'left': '5px',
+    'right': '35px',
+    'padding': '5px',
+    'padding-bottom': '40px'
+}
+
 $(function() {
     $('#app-name').css('line-height',  '42px');
-    $('#app-flavor').css(css_app_flavor);
-
     $('#charts').css('margin-top', '2em');
+
+    $('#editor').css('background-color', '#191919');
+    $('#app-flavor').css(css_app_flavor);
+    $('#editor-data').css(css_editor_data);
+    $('#editor-data-wrapper').css(css_editor_data_wrapper);
 });
