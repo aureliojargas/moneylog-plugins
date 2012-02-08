6360bfc7eb845487a146ce8e66e4d30460eac21f
Leandro Voltolino <xupisco@gmail.com>
2012-02-08 13:09:08 -0200

    updated css tweaks supporting file name tab

--- css_tweaks.js
+++ css_tweaks.js
@@ -14,6 +14,17 @@ var css_editor_data = {
     'line-height': '16px',
     'font-size': '12px',
     'padding': '15px',
+    'font-family': 'Monaco'
+}
+
+var css_editor_filename = {
+    'left': '10px',
+    'top': '16px',
+    'line-height': '32px',
+    'padding': '0 15px',
+    'color': '#777',
+    'background-color': '#222',
+    'font-family': 'Monaco'
 }
 
 var css_editor_data_wrapper = {
@@ -21,16 +32,18 @@ var css_editor_data_wrapper = {
     'top': '5px',
     'left': '5px',
     'right': '35px',
-    'padding': '5px',
-    'padding-bottom': '40px'
+    'padding': '40px 5px 40px',
 }
 
 $(function() {
+    //fileName = $('#editor-file-name').html();
     $('#app-name').css('line-height',  '42px');
     $('#charts').css('margin-top', '2em');
 
     $('#editor').css('background-color', '#191919');
     $('#app-flavor').css(css_app_flavor);
     $('#editor-data').css(css_editor_data);
+    $('#editor-file-name').css(css_editor_filename);
+    //$('#editor-file-name').html(fileName.substr(fileName.lastIndexOf('/') + 1, fileName.length));
     $('#editor-data-wrapper').css(css_editor_data_wrapper);
 });
